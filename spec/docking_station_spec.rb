9c5dfff5a795bb65e396a56a07524692f79e2860
justcallmegoyo <justcallmegoyo@gmail.com>
2019-03-06 14:18:56 +0000

    Test for release_bike method on DockingStation class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,8 @@ require './lib/docking_station.rb'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it "returns a bike object when release_bike method is called" do
+    docking_station = DockingStation.new
+    expect(docking_station.release_bike.is_a?(Bike)).to eq true 
+  end
 end
